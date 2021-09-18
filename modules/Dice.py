0c3f6e1eaa5f04fd5e425f6dc3405ba01f8500b3
Elbert Nathanael <elbertwithane@gmail.com>
2021-09-18 16:27:54 +0800

    added roll limit

--- modules/Dice.py
+++ modules/Dice.py
@@ -4,17 +4,21 @@ import random
 #rolls amount of dice in argument
 #returns an array of successes and failures
 def roll(dice_rolls):
-    #get rolls
-    roll_list = []
-    for i in range(dice_rolls):
-        roll_list.append(random.randrange(0,2))
+    limit = 20
+    if dice_rolls < limit:
+        #get rolls
+        roll_list = []
+        for i in range(dice_rolls):
+            roll_list.append(random.randrange(0,2))
 
-    #convert to array of successes and failures
-    success_count = 0
-    for i in roll_list:
-        if i == 1:
-            success_count += 1
-    failures_count =  dice_rolls - success_count
+        #convert to array of successes and failures
+        success_count = 0
+        for i in roll_list:
+            if i == 1:
+                success_count += 1
+        failures_count =  dice_rolls - success_count
 
-    return [success_count, failures_count]
+        return [success_count, failures_count]
+    else:
+        return "woops"
 
